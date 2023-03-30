1a4fb51b7653f80ff2a4ca2a6af81cfccb097d40
Robert Alexander Iatan <robertiatan@gmail.com>
2023-03-30 18:15:51 -0400

    login logic attempt

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,28 @@
-import './App.css'
-import { Routes, Route } from 'react-router-dom'
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
-import About from './components/About/about';
-// import Contact from './components/Contact/c';
-import Footer from './components/Footer/footer';
-import Nav from './components/Nav/nav';
-import Partners from './components/Partners/partners';
-import AddCause from './components/AddCause/addCause';
-import Login from './components/Login/login';
+import About from "./components/About/about";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Footer from "./components/Footer/footer";
+import Nav from "./components/Nav/nav";
+import Partners from "./components/Partners/partners";
+import AddCause from "./components/AddCause/addCause";
+import Login from "./components/Login/login";
 
 export default function App() {
-    return (
-        <div class="App" >
-            <div class="vh-100">
-                <Nav />
-                    <Routes>
-                        <Route path='/' element={<About />} />
-                        <Route path='/partners' element={<Partners />} />
-                        <Route path='/addcause' element={<AddCause />} />
-                        {/* <Route path='/contact' element={<Contact />} /> */}
-                        <Route path='/login' element={<Login />} />
-                    </Routes>
-                <Footer />
-            </div>
-        </div>
-    )
+  return (
+    <div class="App">
+      <div class="vh-100">
+        <Nav />
+        <Routes>
+          <Route path="/" element={<About />} />
+          <Route path="/partners" element={<Partners />} />
+          <Route path="/addcause" element={<AddCause />} />
+          {/* <Route path='/contact' element={<Contact />} /> */}
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </div>
+    </div>
+  );
 }
--- client/src/components/Footer/footer.js
+++ client/src/components/Footer/footer.js
@@ -1,34 +1,24 @@
-import React from 'react';
-import './footer.css';
-
-const styles = {
-    background: {
-        background: 'rgb(78, 115, 140)',
-        color: 'rgb(246, 246, 237)'
-    },
-    links: {
-        color: 'rgb(246, 246, 237)'
-    }
-};
+import React from "react";
+import "./footer.css";
 
 export default function Footer() {
-    return (
-        <div class="fixed-bottom" style={styles.background}>
-            <h4 class="text-center mb-0 mt-4">Footer</h4>
-            <nav class="d-flex justify-content-center">
-                <address class=" m-3">
-                    {/* <i class="fab fa-github fa-lg m-3" style="color:slategray"></i> */}
-                    {/* <a href="" class="text-decoration-none" style={styles.links}>GitHub</a> */}
-                </address>
-                <address class="m-3">
-                    {/* <i class="fab fa-linkedin fa-lg m-3" style="color:slategray"></i> */}
-                    {/* <a href=""class="text-decoration-none" style={styles.links}>LinkedIn</a> */}
-                </address>
-                <address class="m-3">
-                    {/* <i class="fab fa-stackoverflow fa-lg m-3" style="color:slategray"></i> */}
-                    {/* <a href="" class="text-decoration-none" style={styles.links}>stackoverflow</a> */}
-                </address>
-            </nav>
-        </div>
-    )
+  return (
+    <div class="footerDiv">
+      <h4 class="footerH4">Footer</h4>
+      <nav class="navFooter">
+        <address class="footerLinks">
+          {/* <i class="fab fa-github fa-lg m-3" style="color:slategray"></i> */}
+          {/* <a href="" class="text-decoration-none" style={styles.links}>GitHub</a> */}
+        </address>
+        <address class="footerLinks">
+          {/* <i class="fab fa-linkedin fa-lg m-3" style="color:slategray"></i> */}
+          {/* <a href=""class="text-decoration-none" style={styles.links}>LinkedIn</a> */}
+        </address>
+        <address class="footerLinks">
+          {/* <i class="fab fa-stackoverflow fa-lg m-3" style="color:slategray"></i> */}
+          {/* <a href="" class="text-decoration-none" style={styles.links}>stackoverflow</a> */}
+        </address>
+      </nav>
+    </div>
+  );
 }

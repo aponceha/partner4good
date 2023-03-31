5a659057578cfd9be0c31ab03c56c61ac498b809
Madeline Lowes <114893833+MadelineLowes@users.noreply.github.com>
2023-03-31 15:24:59 -0400

    Update index.js

--- server/seeds/index.js
+++ server/seeds/index.js
@@ -1,20 +1,31 @@
 const db = require('../config/connection');
-const {createCategories, removeAllCategories} = require('./categories');
+const { createCategories, removeAllCategories } = require('./categories');
+const { createCauses, removeAllCauses } = require('./causes');
+const { createUsers, removeAllUsers } = require('./users');
 
 // Should erase everything in the database before seeding
  
 const runSeeds = async () => {
-    // delete seeded causes
     // delete seeded users
-    // seed users
+    console.log("=========== Removing Users ===========");
+    await removeAllUsers();
+    // delete seeded causes
+    console.log("=========== Removing Causes ===========");
+    await removeAllCauses();
+    // delete seeded categories
     console.log("=========== Removing Categories ===========");
     await removeAllCategories();
+    // seed users
+    console.log("=========== Seeding Users ===========");
+    await createUsers();
+    // seed causes
+    console.log("=========== Seeding Causes ===========");
+    await createCauses();
+    // seed categories
     console.log("=========== Seeding Categories ===========");
     await createCategories();
-    // seed causes
-    // seed causes
+    
 }
-
 
 db.once('open', async () => {
     console.log("=========== Start Seeding ===========");
